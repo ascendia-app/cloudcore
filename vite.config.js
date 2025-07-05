1f5cce8705aaca06114f4b97075bfdf404cce10b
Ayesha <cuteandpotato12345@gmail.com>
2025-07-05 23:25:56 +0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/cloudcore/', // 👈 IMPORTANT: use the exact name of your GitHub repo
+  plugins: [react()],
 })
