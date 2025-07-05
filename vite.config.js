a39a7b0e845aa5afbfc1a18d8b8f51ea91901b71
Ayesha <cuteandpotato12345@gmail.com>
2025-07-05 19:11:08 +0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cloudcore/', // <- MUST match your repo name
+  base: '/cloudcore/', // ⚠️ must match your GitHub repo name exactly
 });
